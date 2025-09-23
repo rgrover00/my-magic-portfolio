544b8c25baf27ef6c60a8524f0556c0b69bda480
Ronnie Grover <142832849+rgrover00@users.noreply.github.com>
2025-09-23 06:50:55 -0400

    Update content.js v4

--- src/resources/content.js
+++ src/resources/content.js
@@ -52,7 +52,7 @@ const home = {
   label: "Home",
   title: `${person.name} - Look for what you can't see`,
   description: `Portfolio website showcasing my work as a ${person.role}`,
-  headline: <>Hello, World🌎! I&apos;m Ronnie</>,
+  headline: <>Hello, World 🌎</>,
   featured: {
     display: true,
     title: <>Recent project: <strong className="ml-4">2D Grover Journey</strong></>,
@@ -60,7 +60,7 @@ const home = {
   },
   subline: (
     <>
-      Dad developer and network engineer. I help secure, troubleshoot, and build networks. After hours, I dive into web development, self-hosted tools, and the occasional lego castle.
+      I&apos;m Ronnie, a dad developer and network engineer. I help secure, troubleshoot, and build networks. After hours, I dive into web development, self-hosted tools, and the occasional lego castle.
     </>
   ),
 };
